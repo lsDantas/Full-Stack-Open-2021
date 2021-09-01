793ce0ac69e3b46fde4ebfa9b22a2d4a13864735
Leo <leonardosdantas@protonmail.com>
2021-09-01 16:11:50 -0300

    Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -27,6 +27,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100 + " %";
+
   return (
     <div>
       <Header title="give feedback" />
@@ -40,6 +44,12 @@ const App = () => {
       <Display name="neutral" count={neutral} />
       <br></br>
       <Display name="bad" count={bad} />
+      <br></br>
+      <Display name="all" count={ all } />
+      <br></br>
+      <Display name="average" count={ average } />
+      <br></br>
+      <Display name="positive" count={ positive } />
     </div>
   );
 }
