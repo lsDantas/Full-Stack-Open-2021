0ea5d48af37f6c2606b2c939b8bb2143b2434d6b
Leo <leonardosdantas@protonmail.com>
2021-09-05 18:33:12 -0300

    Exercise 4.5

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -21,27 +21,27 @@ describe('Total likes', () => {
 
     const listWithThreeBlogs = [
         {
-            _id: "5a422a851b54a676234d17f7",
-            title: "React patterns",
-            author: "Michael Chan",
-            url: "https://reactpatterns.com/",
+            _id: '5a422a851b54a676234d17f7',
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
             likes: 7,
             __v: 0,
         },
         {
-            _id: "5a422aa71b54a676234d17f8",
-            title: "Go To Statement Considered Harmful",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-            likes: 5,
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 12,
             __v: 0,
         },
         {
-            _id: "5a422b3a1b54a676234d17f9",
-            title: "Canonical string reduction",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-            likes: 12,
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+            likes: 5,
             __v: 0,
         },
     ];
@@ -59,5 +59,60 @@ describe('Total likes', () => {
     test('of a bigger list is calculated correctly.', () => {
         const result = listHelper.totalLikes(listWithThreeBlogs);
         expect(result).toBe(24);
+    });
+});
+
+describe('Favorite blog', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0,
+        },
+    ];
+
+    const listWithThreeBlogs = [
+        {
+            _id: '5a422a851b54a676234d17f7',
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 7,
+            __v: 0,
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 12,
+            __v: 0,
+        },
+        {
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+            likes: 5,
+            __v: 0,
+        },
+    ];
+
+    test('of empty list of blogs is undefined.', () => {
+        const result = listHelper.favoriteBlog([]);
+        expect(result).toBe(undefined);
+    });
+
+    test('of list with one blog is that blog.', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog);
+        expect(result).toBe(listWithOneBlog[0]);
+    });
+
+    test('of list with three blogs is one with most likes.', () => {
+        const result = listHelper.favoriteBlog(listWithThreeBlogs);
+        expect(result).toBe(listWithThreeBlogs[1]);
     });
 });
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -11,7 +11,19 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+    // Calculate for Valid Lists
+    if (blogs.length > 0) {
+        const reducer = (mostLiked, entry) => (entry.likes > mostLiked.likes ? entry : mostLiked);
+
+        return blogs.reduce(reducer);
+    }
+    console.log("Here");
+    return undefined;
+};
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 };
