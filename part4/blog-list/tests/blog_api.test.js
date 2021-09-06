8b553c2dcc7225de1eb9735346a4777d02990fa2
Leo <leonardosdantas@protonmail.com>
2021-09-06 17:27:55 -0300

    Exercise 4.10

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -25,7 +25,7 @@ beforeEach(async () => {
 }, jestTimeout);
 
 // Tests
-test('Returns correct amount of blog posts in JSON format', async () => {
+test('Returns correct amount of blog posts in JSON format.', async () => {
     const response = await api
         .get('/api/blogs')
         .expect(200)
@@ -34,13 +34,34 @@ test('Returns correct amount of blog posts in JSON format', async () => {
     expect(response.body).toHaveLength(helper.initialBlogs.length);
 }, jestTimeout);
 
-test('Blog entries are identified by "id" rather than "_id"', async () => {
+test('Blog entries are identified by "id" rather than "_id".', async () => {
     const response = await api
         .get('/api/blogs');
 
     const blogs = response.body;
     const blogIds = (blog) => blog.id;
     expect(blogs.map(blogIds)).toBeDefined();
+});
+
+test('Blog entries are correctly added.', async () => {
+    // Add New Entry
+    const blog = new Blog(
+        {
+            title: 'Sample Entry',
+            author: 'No One',
+            url: 'http://www.example.com',
+        },
+    );
+    await blog.save();
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    // Check for Appropriate Length
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+    // Check for Matching Title
+    const titles = blogsAtEnd.map((entry) => entry.title);
+    expect(titles).toContain(blog.title);
 });
 
 // Post-Test Actions
