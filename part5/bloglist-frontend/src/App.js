5bb53a96071d370634e4afb7bf24ea78941cc96c
Leo <leonardosdantas@protonmail.com>
2021-09-08 14:44:09 -0300

    Dumped ESLint after giving linting errors as compiled errors.

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -13,10 +13,59 @@ const App = () => {
     blogService.getAll().then((receivedBlogs) => setBlogs(receivedBlogs));
   }, []);
 
+  // Forms
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username, password
+      });
+    } catch (exception) {
+
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        Username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        Password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map((blog) => <Blog key={blog.id} blog={blog} />)}
+      { user === null
+        ? loginForm()
+        : (
+          <div>
+            <p>
+              {user.name}
+              logged-in
+            </p>
+          </div>
+        )}
+      <br />
+      <div>
+        <h2>blogs</h2>
+        {blogs.map((entry) => <Blog key={entry.id} blog={entry} />)}
+      </div>
     </div>
   );
 };
