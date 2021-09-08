edc1ed0cef7fe2a9a8d933b96c3ee9e72617dd05
Leo <leonardosdantas@protonmail.com>
2021-09-08 13:39:51 -0300

    Add template. Enforce AirBnB style.

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+  // Fetch Blogs from back End
+  useEffect(() => {
+    blogService.getAll().then((receivedBlogs) => setBlogs(receivedBlogs));
+  }, []);
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => <Blog key={blog.id} blog={blog} />)}
+    </div>
+  );
+};
+
+export default App;
