d429f7cf8d75224492d4159d2bf78620644f49c2
Leo <leonardosdantas@protonmail.com>
2021-09-09 16:36:57 -0300

    Exercise 5.14

--- part5/bloglist-frontend/src/components/BlogEntry.test.js
+++ part5/bloglist-frontend/src/components/BlogEntry.test.js
@@ -1,32 +1,62 @@
 import React from 'react';
 
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 
 import BlogEntry from './BlogEntry';
 
-test('BlogEntry renders blog\'s title and author, but not url and number of likes.', () => {
+describe('<BlogEntry />', () => {
 
-  const mockBlog = {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 15,
-    user: {
-      username: 'Blog Owner',
-    },
-  };
-  const mockUser = {
-    username: 'Token User',
-  };
+  test('BlogEntry renders blog\'s title and author, but not url and number of likes.', () => {
 
-  const component = render( <BlogEntry
-    blog={mockBlog}
-    user={mockUser}
-  />);
+    const mockBlog = {
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 15,
+      user: {
+        username: 'Blog Owner',
+      },
+    };
+    const mockUser = {
+      username: 'Token User',
+    };
 
-  expect(component.container).toHaveTextContent(mockBlog.title);
-  expect(component.container).toHaveTextContent(mockBlog.author);
-  expect(component.container).not.toHaveTextContent(mockBlog.url);
-  expect(component.container).not.toHaveTextContent('Likes');
+    const component = render( <BlogEntry
+      blog={mockBlog}
+      user={mockUser}
+    />);
+
+    expect(component.container).toHaveTextContent(mockBlog.title);
+    expect(component.container).toHaveTextContent(mockBlog.author);
+    expect(component.container).not.toHaveTextContent(mockBlog.url);
+    expect(component.container).not.toHaveTextContent('Likes');
+  });
+
+  test('Url and number of likes when button pressed.', () => {
+
+    const mockBlog = {
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 15,
+      user: {
+        username: 'Blog Owner',
+      },
+    };
+    const mockUser = {
+      username: 'Token User',
+    };
+
+    const component = render(<BlogEntry
+      blog={mockBlog}
+      user={mockUser}
+    />);
+
+    const button = component.getByText('Show');
+    fireEvent.click(button);
+
+    expect(component.container).toHaveTextContent(mockBlog.url);
+    expect(component.container).toHaveTextContent('Likes');
+  });
 });
