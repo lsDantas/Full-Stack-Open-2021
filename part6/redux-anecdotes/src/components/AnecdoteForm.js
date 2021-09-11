023c1dbebd22eb85d6ea44ee566a7bf5de8fc0d2
Leo <leonardosdantas@protonmail.com>
2021-09-11 19:35:32 -0300

    Small cosmetic changes to Exercise 6.18

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { createAnecdote } from '../reducers/anecdoteReducer';
-import { setNotification, removeNotification } from '../reducers/notificationReducer';
+import { setNotification } from '../reducers/notificationReducer';
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
--- part6/redux-anecdotes/src/components/AnecdotesList.js
+++ part6/redux-anecdotes/src/components/AnecdotesList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { voteAnecdote } from '../reducers/anecdoteReducer';
-import { setNotification, removeNotification } from '../reducers/notificationReducer';
+import { setNotification } from '../reducers/notificationReducer';
 
 const AnecdotesList = () => {
   const dispatch = useDispatch();
