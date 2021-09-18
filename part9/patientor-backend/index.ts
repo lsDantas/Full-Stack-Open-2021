924655d065e6f6d002e4811814e4fe144e43484a
Leo <leonardosdantas@protonmail.com>
2021-09-18 20:16:26 -0300

    Exercise 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { Request, Response } from 'express';
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req: Request, res: Response) => {
+app.get('/api/ping', (_req: Request, res: Response) => {
   console.log('Someone pinged here.');
   res.send('pong');
 });
